24fe16f5915401e3dd68daadc2a3e216f5dcd396
ortizdavid <ortizaad1994@gmail.com>
2024-09-05 16:38:56 +0100

    Added: view and indexes for employee

--- database/sql/employees/tables.sql
+++ database/sql/employees/tables.sql
@@ -20,26 +20,36 @@ DROP TABLE IF EXISTS employees.employees;
 CREATE TABLE employees.employees(
     employee_id SERIAL PRIMARY KEY,
     identification_type_id INT NOT NULL,
-    nationality INT,
+    country_id INT,
     marital_status_id INT,
     department_id INT,
     job_title_id INT,
     employment_status_id INT NOT NULL,
     first_name VARCHAR(150) NOT NULL,
     last_name VARCHAR(150) NOT NULL,
+    identification_number VARCHAR(30) UNIQUE NOT NULL,
     gender TYPE_GENDER,
     date_of_birth DATE,
-    identification_number VARCHAR(30) UNIQUE NOT NULL,
     unique_id VARCHAR(50) UNIQUE,
     created_at TIMESTAMP DEFAULT NOW(),
     updated_at TIMESTAMP DEFAULT NOW(),
     CONSTRAINT fk_identification FOREIGN KEY(identification_type_id) REFERENCES reference.identification_types(type_id),
-    CONSTRAINT fk_nationality FOREIGN KEY(nationality) REFERENCES reference.countries(country_id),
+    CONSTRAINT fk_country FOREIGN KEY(country_id) REFERENCES reference.countries(country_id),
     CONSTRAINT fk_marital_status FOREIGN KEY(marital_status_id) REFERENCES reference.marital_statuses(status_id),
     CONSTRAINT fk_department FOREIGN KEY(department_id) REFERENCES company.departments(department_id),
     CONSTRAINT fk_job_title FOREIGN KEY(job_title_id) REFERENCES employees.job_titles(job_title_id),
     CONSTRAINT fk_employment_status FOREIGN KEY(employment_status_id) REFERENCES reference.employment_statuses(status_id)
 );
--- Indexes
-CREATE INDEX idx_nataionality ON employees.employees(nationality);
+
+-- Indexes:
+-- name indexes
+DROP INDEX idx_employees_first_name;
+CREATE INDEX idx_employees_first_name ON employees.employees(first_name);
+DROP INDEX idx_employees_last_name;
+CREATE INDEX idx_employees_last_name ON employees.employees(last_name);
+-- Country index
+DROP INDEX IF EXISTS idx_employees_country;
+CREATE INDEX idx_employees_country ON employees.employees(country_id);
+
+
 
--- database/sql/employees/views.sql
+++ database/sql/employees/views.sql
@@ -1,1 +1,30 @@
 -- views for 'employees' schema 
+
+-- View: view_employee_data
+DROP VIEW IF EXISTS employees.view_employee_data;
+CREATE VIEW employees.view_employee_data AS
+SELECT emp.employee_id, emp.unique_id,
+    emp.first_name, emp.last_name,
+    emp.identification_number,
+    emp.gender, emp.date_of_birth,
+    TO_CHAR(emp.created_at, 'YYYY-MM-DD HH24:MI:SS') AS created_at,
+    TO_CHAR(emp.updated_at, 'YYYY-MM-DD HH24:MI:SS') AS updated_at,
+    it.type_id AS identification_type_id, 
+    it.type_name AS identification_type_name,
+    co.country_id, co.country_name,
+    ms.status_id AS marital_status_id,
+    ms.status_name AS marital_status_name,
+    es.status_id AS employment_status_id,
+    es.status_name AS employment_status_name,
+    jt.job_title_id, jt.title_name
+FROM employees.employees emp 
+LEFT JOIN reference.identification_types it ON(it.type_id = emp.identification_type_id)
+LEFT JOIN reference.countries co ON(co.country_id = emp.country_id)
+LEFT JOIN reference.marital_statuses ms ON(ms.status_id = emp.marital_status_id)
+LEFT JOIN reference.employment_statuses es ON(es.status_id = emp.employment_status_id)
+LEFT JOIN company.departments dpt ON(dpt.department_id = emp.department_id)
+LEFT JOIN employees.job_titles jt ON(jt.job_title_id = emp.job_title_id)
+ORDER BY emp.created_at;
+
+
+
