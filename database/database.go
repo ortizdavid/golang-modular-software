5b9f37f06758e72b11d0069e54863bb6e2e496dd
ortizdavid <ortizaad1994@gmail.com>
2024-09-18 13:50:35 +0100

    Refactored: WithTransaction function | made begin and commit private

--- database/database.go
+++ database/database.go
@@ -16,7 +16,7 @@ func NewDatabase(db *gorm.DB) *Database {
 }
 
 // BeginTx starts a transaction
-func (d *Database) BeginTx(ctx context.Context) (*Database, error) {
+func (d *Database) beginTx(ctx context.Context) (*Database, error) {
 	tx := d.DB.WithContext(ctx).Begin()
 	if tx.Error != nil {
 		return nil, tx.Error
@@ -25,29 +25,29 @@ func (d *Database) BeginTx(ctx context.Context) (*Database, error) {
 }
 
 // CommitTx commits the transaction
-func (d *Database) CommitTx() error {
+func (d *Database) commitTx() error {
 	return d.DB.Commit().Error
 }
 
 // RollbackTx rolls back the transaction
-func (d *Database) RollbackTx() error {
+func (d *Database) rollbackTx() error {
 	return d.DB.Rollback().Error
 }
 
 // WithTx runs the provided function within a transaction
 // Rollback if an error occurred
 func (d *Database) WithTx(ctx context.Context, fn func(tx *Database) error) error{
-	tx, err := d.BeginTx(ctx)
+	tx, err := d.beginTx(ctx)
 	if err != nil {
 		return err
 	}
 	err = fn(tx)
 	if err != nil {
-		if rbErr := tx.RollbackTx(); rbErr != nil {
+		if rbErr := tx.rollbackTx(); rbErr != nil {
 			return rbErr // rollback Error
 		}
 		return err // function error
 	}
-	return tx.CommitTx()
+	return tx.commitTx()
 }
 
--- modules/shared/repositories/base_repository_transaction.go
+++ modules/shared/repositories/base_repository_transaction.go
@@ -6,11 +6,6 @@ import (
 	"github.com/ortizdavid/golang-modular-software/database"
 )
 
-// This method can be used in services and other packages
-func (repo *BaseRepository[T]) BeginTransaction(ctx context.Context) (*database.Database, error) {
-	return repo.db.BeginTx(ctx)
-}
-
 // WithTransaction executes the provided function within a transaction.
 // This method can be used in services and other packages that require transactional integrity
 func (repo *BaseRepository[T]) WithTransaction(ctx context.Context, fn func(tx *database.Database) error) error {
