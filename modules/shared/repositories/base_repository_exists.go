bc2c2bb3993a1f182f60fae7ab1a54ab264e0856
ortizdavid <ortizaad1994@gmail.com>
2024-09-18 09:03:50 +0100

    Refactored base repository | Removed entities Dependency

--- modules/shared/repositories/base_repository_exists.go
+++ modules/shared/repositories/base_repository_exists.go
@@ -6,8 +6,6 @@ import "context"
 func (repo *BaseRepository[T]) ExistsField(ctx context.Context, field string, value interface{}) (bool, error) {
 	var count int64
 	var entity T
-	/*tableName := entity.TableName()  // Get table name for the entity
-	result := repo.db.WithContext(ctx).Table(tableName).Where(field+" = ?", value).Count(&count)*/
 	result := repo.db.WithContext(ctx).Model(&entity).Where(field+" = ?", value).Count(&count)
 	if result.Error != nil {
 		return false, result.Error
--- modules/shared/repositories/base_repository_find_all.go
+++ modules/shared/repositories/base_repository_find_all.go
@@ -5,9 +5,6 @@ import "context"
 // FindAllLimit retrieves entities with pagination (limit and offset).
 func (repo *BaseRepository[T]) FindAllLimit(ctx context.Context, limit int, offset int) ([]T, error) {
 	var entities []T
-	//var entity T
-	/*tableName := entity.TableName()  // Get table name for the entity
-	result := repo.db.WithContext(ctx).Table(tableName).Limit(limit).Offset(offset).Find(&entities)*/
 	result := repo.db.WithContext(ctx).Model(&entities).Limit(limit).Offset(offset).Find(&entities)
 	return entities, result.Error
 }
@@ -15,7 +12,6 @@ func (repo *BaseRepository[T]) FindAllLimit(ctx context.Context, limit int, offs
 // FindAllOrdered retrieves entities ordered by a specific field.
 func (repo *BaseRepository[T]) FindAllOrdered(ctx context.Context, fieldAnOrder string) ([]T, error) {
 	var entities []T
-	//result := repo.db.WithContext(ctx).Order(fieldAnOrder).Find(&entities)
 	result := repo.db.WithContext(ctx).Order(fieldAnOrder).Find(&entities)
 	return entities, result.Error
 }
